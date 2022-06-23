9181538f5c16e6a974eff06086dea2cd99960e0f
Supreeth JR <65713127+Supreeth-JR@users.noreply.github.com>
2022-06-23 22:25:57 +0530

    Delete IBatteryState.cs

--- IBatteryState.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Checker
-{
-    public interface IBatteryState
-    {
-        bool CheckTemperature(float temperature);
-        bool CheckStateOfCharge(float stateOfCharge);
-        bool CheckChargeRate(float chargeRate);
-    }
-}
